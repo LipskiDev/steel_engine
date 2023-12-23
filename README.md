4147a2cf7edff9880eb46fc4234401a5e6edf3db
Maximilian.Lipski <maximilian.lipski@hhu.de>
2023-12-23 19:00:11 +0100

    Added Texture to Rectangle

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 > 3D Render Engine used to learn Computer Graphics, implemented in C++ and OpenGL
 
+**currently supporting:**
+* Rendering Simple Shapes
 ![](img/rainbow_triangle.png)
+
+
+
+<br/><br/>
+* Rendering Textured Shapes
+![](img/textured_rectangle.png)
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 
 #include "Shader.h"
+#include "stb_image.h"
 
 // Adjusts OpenGL's Viewport size based on window size
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
@@ -55,37 +56,77 @@ int main() {
     // Setup Shader
     Shader shader("../shaders/vertexShader.glsl", "../shaders/fragmentShader.glsl");
 
-    // Triangle Vert Positions and Colors
+    // Rectagle Vert Positions and Colors
     float vertices[] = {
-        // positions        // colors
-        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // left  
-         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // right 
-         0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f  // top   
-    }; 
+        // positions        // colors           // texture coords
+        0.5f, 0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+        0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -0.5f, 0.5f, 0.0f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f // top left
+    };
 
-    GLuint VBO, VAO;
+    int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    GLuint VBO, VAO, EBO;
 
     // Create Buffer and Array Objects
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     // Bind Buffer and Array Objects for subsequent use
     glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     // Position Attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,  6 * sizeof(float), nullptr);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,  8 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
 
     // Color Attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) (3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    // Texture Attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) (6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
     glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+
+
+
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    
+    int width, height, nrChannels;
+
+    unsigned char *data = stbi_load("../img/container.jpg", &width, &height, &nrChannels, 0);
+
+    if(data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load Texture.\n";
+    }
+
+    stbi_image_free(data);
 
 
 
@@ -102,11 +143,15 @@ int main() {
 
         // Draw Triangle
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
